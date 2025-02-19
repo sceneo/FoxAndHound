4ca4378d77e0d2840596d95bbf2682aad040324a
Tobias Kunz <Tobias.Kunz@bkw.ch>
2025-02-19 07:58:49 +0100

    slider

--- frontend/src/app/rating-slider/rating-slider.component.ts
+++ frontend/src/app/rating-slider/rating-slider.component.ts
@@ -1,5 +1,6 @@
 import {Component, EventEmitter, Output} from '@angular/core';
 import {MatSlider, MatSliderThumb} from '@angular/material/slider';
+import {elementAt} from 'rxjs';
 
 @Component({
   selector: 'app-rating-slider',
@@ -20,5 +21,26 @@ export class RatingSliderComponent {
     this.ratingChange.emit(value);
   }
 
+  formatLabel(value: number): string {
+    return `${value} %`;
+    // TODO check if we prefer that:
+    /**
+    if (value === 0) {
+      return "not fulfilled"
+    } else if (value === 25) {
+      return "slightly fulfilled"
+    } else if (value === 50) {
+      return "partially fulfilled"
+    } else if (value === 75) {
+      return "mostly fulfilled"
+    } else if (value === 100) {
+      return "fully fulfilled"
+    } else if (value === 125) {
+      return "overly fulfilled"
+    } else {
+      return "not rated"
+    }
+      **/
+  }
 
 }
